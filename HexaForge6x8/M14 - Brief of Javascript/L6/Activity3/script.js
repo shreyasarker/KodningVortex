4e33634be60d830cf4f33bc8dbbb89278de2c946
Shreya Sarker <shreya.sarker@gmail.com>
2025-07-18 03:30:09 +0600

    M14 - Brief of Javascript

---
+++ HexaForge6x8/M14 - Brief of Javascript/L6/Activity3/script.js
@@ -0,0 +1,7 @@
+const clearScreen = async () => {
+    document.getElementById("result").value = "";
+}
+
+const setScreenValue = async (value) => {
+    document.getElementById("result").value += value;
+}
